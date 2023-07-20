1cba90b2de28ba06024f3444ec0c9718a2cd85e9
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-07-20 17:49:22 -0300

    Spring project, settings and layer structure

---
+++ src/main/java/com/devsuperior/lazyloading/LazyloadingApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.lazyloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyloadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyloadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/lazyloading/LazyloadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.lazyloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyloadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
